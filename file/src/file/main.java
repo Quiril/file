5b93b53771501839613d729a005ab03bcf802dff
ThePc <ThePc@WIN-E3PFJSN6BDH>
2020-06-29 19:47:50 +0300

    hmmm

--- file/src/file/main.java
+++ file/src/file/main.java
@@ -2,24 +2,40 @@ package file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
+		
 		File file = new File("data/person.txt");
+//		try {
+//			Scanner in = new Scanner(file);
+//			String f1 = in.next();
+//			String f2 = in.next();
+//			String f3 = in.next();
+//			int f4 = in.nextInt();
+//			System.out.println(f1+" "+f2+" "+f3+" "+f4);
+//		} catch (FileNotFoundException e) {
+//
+//			e.printStackTrace();
+//		}
 		try {
-			Scanner in = new Scanner(file);
+			
+			FileWriter fiel = new FileWriter("data/person.txt");
+			Scanner in = new Scanner(System.in);
 			String f1 = in.next();
 			String f2 = in.next();
 			String f3 = in.next();
 			int f4 = in.nextInt();
-			System.out.println(f1+" "+f2+" "+f3+" "+f4);
-		} catch (FileNotFoundException e) {
-
+			fiel.write(f1+" "+f2+" "+f3+" "+f4);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 	}
 
 }
