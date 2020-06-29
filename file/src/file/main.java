59bbac6350a2e492047b28a6cb604bb0981076a0
ThePc <ThePc@WIN-E3PFJSN6BDH>
2020-06-29 20:21:37 +0300

    works

--- file/src/file/main.java
+++ file/src/file/main.java
@@ -11,30 +11,32 @@ public class main {
 	public static void main(String[] args) {
 		
 		File file = new File("data/person.txt");
-//		try {
-//			Scanner in = new Scanner(file);
-//			String f1 = in.next();
-//			String f2 = in.next();
-//			String f3 = in.next();
-//			int f4 = in.nextInt();
-//			System.out.println(f1+" "+f2+" "+f3+" "+f4);
-//		} catch (FileNotFoundException e) {
-//
-//			e.printStackTrace();
-//		}
+
 		try {
 			
-			FileWriter fiel = new FileWriter("data/person.txt");
+			FileWriter fiel = new FileWriter(file);
 			Scanner in = new Scanner(System.in);
 			String f1 = in.next();
 			String f2 = in.next();
 			String f3 = in.next();
 			int f4 = in.nextInt();
 			fiel.write(f1+" "+f2+" "+f3+" "+f4);
+			fiel.close();
+			in.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		try {
+		Scanner in = new Scanner(file);
+		String f1 = in.next();
+		String f2 = in.next();
+		String f3 = in.next();
+		int f4 = in.nextInt();
+		System.out.println(f1+" "+f2+" "+f3+" "+f4);
+	} catch (FileNotFoundException e) {
+
+		e.printStackTrace();
+	}
 		
 	}
 
